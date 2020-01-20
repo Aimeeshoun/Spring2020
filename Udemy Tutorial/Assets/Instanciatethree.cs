d90d9e4260b858faffa7857035cdc9874a5e035c
Aimee <10893245@my.uvu.edu>
2020-01-20 09:26:24 -0700

    Experimenting with Vertical Movement and instantiate Scripts

---
+++ Udemy Tutorial/Assets/Instanciatethree.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instanciatethree : MonoBehaviour
+
+    {
+        public GameObject prefab;
+
+        void Start()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Instantiate(prefab, new Vector3(i * 6.0F, 3, 0), Quaternion.identity);
+            }
+        }
+
+        void Update()
+
+        {
+            float verticalInput = Input.GetAxis("Vertical");
+            transform.Translate(Vector3.up * 3 * verticalInput * Time.deltaTime);
+        }
+    }
+
+
