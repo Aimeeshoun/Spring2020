a4843c9580049cf73c9bc50fcd0a7e967292cd88
Aimee <10893245@my.uvu.edu>
2020-01-20 08:53:26 -0700

    Experiment with instantiate and Movement

---
+++ Udemy Tutorial/Assets/Instanciatetwo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instanciatetwo : MonoBehaviour
+
+    {
+        public GameObject prefab;
+
+        void Start()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Instantiate(prefab, new Vector3(i * 5.0F, 0, 0), Quaternion.identity);
+                
+            }
+            
+            
+        }
+
+        void Update()
+        {
+            transform.Translate(Vector3.up * 5 * Time.deltaTime);
+        }
+    }
