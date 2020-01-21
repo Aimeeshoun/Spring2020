5e59398c5dcbc9b78d56c47380ad07f3224eb41a
Aimee <10893245@my.uvu.edu>
2020-01-21 11:42:04 -0700

    Experimenting with Movement, transition.position, transition.translate

---
+++ Udemy Tutorial/Assets/Instanciate5.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instanciate5 : MonoBehaviour
+{
+   void Start()
+   {
+      transform.position=new Vector3(0.5f, 2f, 0f);
+   }
+
+   private void Update()
+   {
+      transform.Translate(Vector3.back*5*Time.deltaTime);
+   }
+}
---
+++ Udemy Tutorial/Assets/Instanciatefour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instanciatefour : MonoBehaviour
+{
+    public GameObject prefab;
+
+    void Start()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(prefab, new Vector3(i * 3.0F, 5, 0), Quaternion.identity);
+        }
+    }
+
+    void Update()
+
+    {
+        float HorizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * 9 * HorizontalInput * Time.deltaTime);
+    }
+}
+
+
